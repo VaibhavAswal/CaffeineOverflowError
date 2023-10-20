f6bb3561680b75aaba105e311f2ef655576ee9ff
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-10-21 00:10:06 +0530

    fix linting errors

---
+++ src/app/user/checkout/page.tsx
@@ -0,0 +1,5 @@
+const Checkout = () => {
+  return <div>page</div>;
+};
+
+export default Checkout;
--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,5 +1,5 @@
-const profilePage = () => {
+const ProfilePage = () => {
   return <div>page</div>;
 };
 
-export default profilePage;
+export default ProfilePage;

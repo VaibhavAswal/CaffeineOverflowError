4f83fe117b4cee9757b2c3fbf9b207d6f66ee068
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-10-21 10:34:52 +0530

    added top loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import TopBar from "@/components/topBar/topBar";
 
 const poppinsFont = Poppins({
   subsets: ["latin-ext"],
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppinsFont.className}>{children}</body>
+      <body className={poppinsFont.className}>
+        <TopBar />
+        {children}
+      </body>
     </html>
   );
 }

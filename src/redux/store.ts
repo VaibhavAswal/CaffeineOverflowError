5dcec5039a64110e0b9c3bc363be1cf99febd894
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-10-22 14:00:55 +0530

    added useAppSelector

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 import authReducer from "./features/authSlice";
 
 export const store = configureStore({
@@ -9,3 +10,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

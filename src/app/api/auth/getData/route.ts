df9432a4bfd4ef85011f4d04a252e297e01bb9e9
Vaibhav Aswal <vbhv.aswal@gmail.com>
2023-10-22 14:00:21 +0530

    removed passing sensitive data in getdata api

--- src/app/api/auth/getData/route.ts
+++ src/app/api/auth/getData/route.ts
@@ -9,7 +9,12 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDataFromToken(request);
-    const user = await User.findOne({ _id: userId }).select("-password");
+    const user = await User.findOne({ _id: userId }).select(
+      "-password -createdAt -updatedAt -__v -verifyToken -verifyTokenExpiry -forgotPasswordToken -forgotPasswordTokenExpiry"
+    );
+    if (!user) {
+      return NextResponse.json({ error: "invalid token" }, { status: 400 });
+    }
     return NextResponse.json(
       {
         mesaaage: "User found",
